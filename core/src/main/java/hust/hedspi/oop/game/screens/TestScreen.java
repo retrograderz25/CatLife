e1d220769c35f3da6cef1e2eddf44ad738f23726
anhnguyenicyqs <anhnguyenforwork123@gmail.com>
2026-05-26 14:33:57 +0700

    game trom meo

--- core/src/main/java/hust/hedspi/oop/game/screens/TestScreen.java
+++ core/src/main/java/hust/hedspi/oop/game/screens/TestScreen.java
@@ -18,6 +18,7 @@ import hust.hedspi.oop.game.minigames.cao_mong.CaoMongMinigame;
 import hust.hedspi.oop.game.minigames.tim_tieu_tam.TimTieuTamMinigame;
 import hust.hedspi.oop.game.minigames.thoat_khoi_cong.ThoatKhoiCongMinigame;
 import hust.hedspi.oop.game.minigames.tron_kim_tiem.TronKimTiemMinigame;
+import hust.hedspi.oop.game.minigames.trom_meo.TromMeoMinigame;
 import hust.hedspi.oop.game.utils.IObserver;
 
 public class TestScreen implements Screen, IObserver {
@@ -82,6 +83,11 @@ public class TestScreen implements Screen, IObserver {
             ScreenManager.getInstance().pushScreen(new MinigameScreen(new TronKimTiemMinigame()));
             return;
         }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.H)) {
+            ScreenManager.getInstance().pushScreen(new MinigameScreen(new TromMeoMinigame()));
+            return;
+        }
+
 
         // UI update based on flags
         if (uiNeedsUpdate) {
@@ -114,7 +120,7 @@ public class TestScreen implements Screen, IObserver {
             dialogFont.draw(batch, "Máu: " + GameManager.getInstance().getPlayer().getHp() + " Năng lượng: " + GameManager.getInstance().getPlayer().getEnergy() + " Hành vi: " + GameManager.getInstance().getPlayer().getCurrentState().getClass().getSimpleName(), 50, Gdx.graphics.getHeight() - 350);
         }
 
-        dialogFont.draw(batch, "Nhấn [M] Rhythm | [C] CaoMong | [E] ThoatCong | [B] BathGame | [T] TimTieuTam | [V] TronKimTiem | [WASD] Di chuyển", 50, 100);
+        dialogFont.draw(batch, "Nhấn [M] Rhythm | [C] CaoMong | [E] ThoatCong | [B] BathGame | [T] TimTieuTam | [V] TronKimTiem | [H] TromMeo | [WASD] Di chuyển", 50, 100);
         
         batch.end();
     }
