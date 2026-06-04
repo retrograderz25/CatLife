47fcce649f184d0226ef5e77fd0274177a237841
Hoàng Bình Phương <retrograderz@gmail.com>
2026-06-05 00:19:53 +0700

    feat: add game logo

--- lwjgl3/src/main/java/hust/hedspi/oop/game/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/hust/hedspi/oop/game/lwjgl3/Lwjgl3Launcher.java
@@ -31,7 +31,8 @@ public class Lwjgl3Launcher {
         configuration.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         //// They can also be loaded from the root of assets/ .
-        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        // configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        configuration.setWindowIcon("images\\Logo\\catlifelogo.png");
 
         //// This could improve compatibility with Windows machines with buggy OpenGL drivers, Macs
         //// with Apple Silicon that have to emulate compatibility with OpenGL anyway, and more.
